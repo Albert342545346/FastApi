aac7950342e3065e53a18cd0207e87b0f86e4fdc
Albert342545346 <112861843+Albert342545346@users.noreply.github.com>
2026-02-18 23:53:34 +0300

    1

--- control1.py
+++ control1.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import FileResponse
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, field_validator
+from typing import List
 
 app = FastAPI()
 
@@ -37,7 +38,7 @@ async def users():
     return User(name="Ваше Имя и Фамилия", id=1)
 
 
-class UserWithAge(BaseModel):
+class UserWithAge(BaseModel): # 1.5
     name: str
     age: int
 
@@ -48,3 +49,45 @@ async def user(user: UserWithAge):
         "age": user.age,
         "is_adult": user.age >= 18
     }
+
+
+class Feedback(BaseModel): # 2.1
+    name: str
+    message: str
+
+feedback_storage: List[dict] = []
+
+@app.post("/feedback")
+async def feedback_message(feedback: Feedback):
+    feedback_storage.append({
+        "name": feedback.name,
+        "message": feedback.message
+    })
+    
+    return {"message": f"Feedback received. Thank you, {feedback.name}."}
+
+
+class Feedbacks(BaseModel):
+    name: str = Field(..., min_length=2, max_length=50)
+    message: str = Field(..., min_length=10, max_length=500)
+
+feedback_storage1: List[dict] = []
+words = ["кринж", "рофл", "вайб"]
+
+@field_validator("message")
+@classmethod
+def check_feedbacks_message(cls, v: str):
+    message_lower = v.lower()
+    for word in words:
+        if word in message_lower:
+            raise ValueError("Использование недопустимых слов")
+    return v
+
+@app.post("/feedbacks")
+async def feedbacks_message(feedbacks: Feedbacks):
+    feedback_storage1.append({
+        "name": feedbacks.name,
+        "message": feedbacks.message
+    })
+    
+    return {"message": f"Спасибо, {feedbacks.name}! Ваш отзыв сохранён."}
